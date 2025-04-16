d419ba5e6fbd0e340b5fc66c7c9f1752f31e3a25
Akshay-Miyatra <mrakmiyatra@gmail.com>
2025-04-16 11:34:59 +0530

    Add files via upload

---
+++ chart.py
@@ -0,0 +1,49 @@
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# df=pd.read_excel('Book1.xlsx')
+# print(df)
+
+# x=df['year']
+# y=df['book_sales']
+
+# plt.bar(x,y)
+# plt.xlabel('year')
+# plt.ylabel('units')
+# plt.title('Xyz books')
+# plt.show()
+
+#pie chart
+# import matplotlib.pyplot as plt
+# sales_per = [2,2,22,20,20]
+# com = ['nataraj1','natraj2','natraj3','natraj4','natraj5']
+# plt.pie(sales_per,labels=com)
+# plt.title('pie chart')
+# plt.show()
+
+#linew chart
+# import matplotlib.pyplot as plt
+# years = [2020,2021,2022,2023,2024]
+# inc_decr_sales=[2,67,22,34,56]
+# inc_decr_sales1=[55,30,90,20,100]
+# plt.plot(inc_decr_sales)
+# plt.plot(inc_decr_sales1)
+# plt.show()
+
+#scatter plot
+# import matplotlib.pyplot as plt
+# inc_decr_sales=[2,67,22,34,56]
+# inc_decr_sales1=[55,30,90,20,100]
+# plt.scatter(inc_decr_sales,inc_decr_sales1)
+# plt.show()
+
+#area plot
+#area plot is same as te line chart but in area plot area is fill by color that is the main difference between an area plot and a line chart
+# import matplotlib.pyplot as plt
+# import numpy as np
+# sales1=[6,5,4,3,2]
+# # x=range(1,6)
+# sales=[10,20,30,40,50]
+# plt.fill_between(sales1,sales)
+# plt.show()
+
+
